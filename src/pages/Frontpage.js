ad3a574e9d0009a864c485ad276bd0fa02ad0c29
Steffen Hansen <stefh14@student.sdu.dk>
2017-01-02 22:14:05 +0100

    Added socket.io name to frontpage

--- src/pages/Frontpage.js
+++ src/pages/Frontpage.js
@@ -9,7 +9,7 @@ export default class Frontpage extends Component {
             <Grid>
                 <div className="page-header">
                     <h1>Welcome to bidding.io</h1>
-                    <p className="lead">Basic auction system using REST and WebSockets to update data and React as web-app framework.</p>
+                    <p className="lead">Basic auction system using REST and Socket.io to update data and React as web-app framework.</p>
                 </div>
                 <AuctionPanel/>
             </Grid>
